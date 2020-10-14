b51310055487bd274911cfabfd40d90b3f1d28dd
Ibrahim <ibra351996@gmail.com>
2020-10-14 21:04:12 +0200

    Simulation added

--- ICU/LCD_Cfg.h
+++ ICU/LCD_Cfg.h
@@ -11,16 +11,16 @@
 
 #include "DIO.h"
 
-#define  LCD_8BIT_CMD_PORT     DIO_PORTC
+#define  LCD_8BIT_CMD_PORT     DIO_PORTB
 #define  LCD_8BIT_DATA_PORT    DIO_PORTA
 
-#define  LCD_4BIT_CMD_PORT     DIO_PORTC
+#define  LCD_4BIT_CMD_PORT     DIO_PORTB
 #define  LCD_4BIT_DATA_PORT    DIO_PORTA
 
 
-#define  LCD_RS_PIN            DIO_PIN2
-#define  LCD_RW_PIN            DIO_PIN3
-#define  LCD_E_PIN             DIO_PIN4
+#define  LCD_RS_PIN            DIO_PIN4
+#define  LCD_RW_PIN            DIO_PIN5
+#define  LCD_E_PIN             DIO_PIN6
 
 #define  LCD_D0                DIO_PIN0
 #define  LCD_D1                DIO_PIN1
